e15d9816f6a5efa57fd1105e0763bc9727bc8bb1
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-06-05 15:09:30 -0400

    tracker update

--- client/src/components/ProgressTracker.js
+++ client/src/components/ProgressTracker.js
@@ -15,6 +15,25 @@ function ProgressTracker() {
       .catch((error) => console.error("Error fetching workouts:", error));
   }, []);
 
+  const handleExerciseCompletion = (workoutId, exerciseId) => {
+    // Send a request to the API to mark the exercise as completed for the user
+    // You can use the workoutId and exerciseId to identify the specific exercise
+
+    // Example code:
+    fetch(`/workout-plans/${workoutId}/exercises/${exerciseId}/complete`, {
+      method: "POST",
+      // Additional request options (e.g., headers, body) can be added here
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response if needed
+        console.log(data);
+      })
+      .catch((error) =>
+        console.error("Error marking exercise completion:", error)
+      );
+  };
+
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -30,7 +49,24 @@ function ProgressTracker() {
             <li key={workout.id}>
               <h3>{workout.title}</h3>
               <p>{workout.description}</p>
-              <button>Track Progress</button>
+              {workout.exercises.length === 0 ? (
+                <p>No exercises available for this workout.</p>
+              ) : (
+                <ul>
+                  {workout.exercises.map((exercise) => (
+                    <li key={exercise.id}>
+                      <p>{exercise.title}</p>
+                      <button
+                        onClick={() =>
+                          handleExerciseCompletion(workout.id, exercise.id)
+                        }
+                      >
+                        Mark as Completed
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           ))}
         </ul>
