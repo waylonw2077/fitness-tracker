967ec9f32f47b098f8cbc3e746cc65a8090f9468
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-06-05 11:59:33 -0400

    basic models and app

--- server/app.py
+++ server/app.py
@@ -8,9 +8,46 @@ from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import User, WorkoutPlan
 
-# Views go here!
+# Routes
 
+# User endpoints
+class UserResource(Resource):
+    def get(self, user_id):
+        user = User.query.get(user_id)
+        if user:
+            return user.to_dict()
+        return {'message': 'User not found'}, 404
+
+    def post(self):
+        # Create a new user based on the request data
+        data = request.get_json()
+        user = User(username=data['username'], email=data['email'], password=data['password'])
+        db.session.add(user)
+        db.session.commit()
+        return {'message': 'User created successfully'}, 201
+
+api.add_resource(UserResource, '/users', '/users/<int:user_id>')
+
+# WorkoutPlan endpoints
+class WorkoutPlanResource(Resource):
+    def get(self, workout_plan_id):
+        workout_plan = WorkoutPlan.query.get(workout_plan_id)
+        if workout_plan:
+            return workout_plan.to_dict()
+        return {'message': 'Workout plan not found'}, 404
+
+    def post(self):
+        # Create a new workout plan based on the request data
+        data = request.get_json()
+        workout_plan = WorkoutPlan(title=data['title'], description=data['description'])
+        db.session.add(workout_plan)
+        db.session.commit()
+        return {'message': 'Workout plan created successfully'}, 201
+
+api.add_resource(WorkoutPlanResource, '/workout-plans', '/workout-plans/<int:workout_plan_id>')
+
+# Main entry point
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,71 @@
 from sqlalchemy_serializer import SerializerMixin
-
 from config import db
 
-# Models go here!
+# User model
+class User(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    
+    # Relationships
+    workout_progress = db.relationship('WorkoutProgress', backref='user', lazy=True)
+    favorite_workouts = db.relationship('FavoriteWorkout', backref='user', lazy=True)
+
+# WorkoutPlan model
+class WorkoutPlan(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    
+    # Relationships
+    exercises = db.relationship('Exercise', backref='workout_plan', lazy=True)
+    favorite_workouts = db.relationship('FavoriteWorkout', backref='workout_plan', lazy=True)
+
+# Exercise model
+class Exercise(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    workout_plan_id = db.Column(db.Integer, db.ForeignKey('workout_plan.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    sets = db.Column(db.Integer)
+    reps = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    # Relationships
+    workout_plan = db.relationship('WorkoutPlan', backref=db.backref('exercises', order_by=id), lazy=True)
+    workout_progress = db.relationship('WorkoutProgress', backref='exercise', lazy=True)
+
+# WorkoutProgress model
+class WorkoutProgress(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'), nullable=False)
+    date = db.Column(db.Date)
+    duration = db.Column(db.Integer)
+    sets_completed = db.Column(db.Integer)
+    reps_completed = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    # Relationships
+    user = db.relationship('User', backref=db.backref('workout_progress', order_by=id), lazy=True)
+    exercise = db.relationship('Exercise', backref=db.backref('workout_progress', order_by=id), lazy=True)
+
+# FavoriteWorkout model
+class FavoriteWorkout(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    workout_plan_id = db.Column(db.Integer, db.ForeignKey('workout_plan.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    # Relationships
+    user = db.relationship('User', backref=db.backref('favorite_workouts', order_by=id), lazy=True)
+    workout_plan = db.relationship('WorkoutPlan', backref=db.backref('favorite_workouts', order_by=id), lazy=True)
