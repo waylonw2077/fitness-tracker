d3d929c1dc5596b1499207e79d5554f1d7c88032
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-06-07 10:40:26 -0400

    fixed login page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -6,6 +7,14 @@ function Home() {
       <h1>Welcome to Fitness Progress Tracker</h1>
       <p>Track your fitness progress and achieve your goals!</p>
       <p>Sign up or log in to get started.</p>
+
+      <Link to="/register">
+        <button>Sign Up</button>
+      </Link>
+
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
     </div>
   );
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Login() {
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Create an object with email and password
+    // Create an object with username, email, and password
     const credentials = {
+      username: username,
       email: email,
       password: password,
     };
@@ -21,8 +25,12 @@ function Login() {
       .then((response) => {
         // Login successful
         setMessage(response.data.message);
+        setUsername("");
         setEmail("");
         setPassword("");
+
+        // Redirect to workout plans page
+        history.push("/workout-plans");
       })
       .catch((error) => {
         // Login failed
@@ -34,6 +42,15 @@ function Login() {
     <div>
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
         <div>
           <label htmlFor="email">Email:</label>
           <input
