83d9f692ff4eccb01d943360f5296f331a8ef64d
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-06-07 10:23:12 -0400

    fixed routes error

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 ReactDOM.render(
   <React.StrictMode>
